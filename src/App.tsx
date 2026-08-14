22ab339fea9018b020f674b446ce3d484d1fc5d7
Melanie <135548773+EmlMeml@users.noreply.github.com>
2026-08-14 11:59:07 +0200

    editor um context erweitert

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import './App.css'
 import RichTextEditor from './custom/editor/Editor';
+import type { StoryContext } from "./types/story";
 
 function App() {
-
+  const storyContext: StoryContext = {
+    referenceDate: "2026-08-14",
+  };
 
   return (
     <>
@@ -16,7 +19,7 @@ function App() {
       </Grid>
       <Grid container spacing={2}>
         <Grid size={12} sx={{display: 'flex', justifyContent: 'center', alignItems:'center', height:'720px'}}>
-          <RichTextEditor></RichTextEditor>
+          <RichTextEditor context={storyContext}></RichTextEditor>
         </Grid>
       </Grid>
     </Box>
